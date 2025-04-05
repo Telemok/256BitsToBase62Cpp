06f9a70a5284af2473e73451544d9a4d20cbc2bd
Telemok <tlevelpack@yandex.ru>
2025-04-06 05:07:44 +0700

    Commit again

---
+++ 256BitsToBase62/256BitsToBase62.cpp
@@ -0,0 +1,189 @@
+/**
+ * @file    256BitsToBase62.cpp
+ * @brief
+ * convert 256 bits to ([0-9A-Za-z]){43} with speed 1M/sec (1 thread)
+ * convert ([0-9A-Za-z]){43} => 256 bits with speed 1M/sec (1 thread)
+ * this is 3-hours written algorithm, not library
+ * @author  Dmitrii Arshinnikov <www.telemok.com@gmail.com> github.com/Telemok
+ * https://github.com/Telemok/256BitsToBase62Cpp.git
+ * @version 0.0.1
+ * @date 2025-04-06
+ *
+ @verbatim
+
+ Licensed under the Apache License, Version 2.0(the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ @endverbatim
+ */
+
+
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <conio.h>
+using namespace std;
+
+/* Fast way to serialise any 256 bits to 43 bytes of [0-9A-Za-z] and return back. */
+struct alignas(32) Data256 {
+	uint64_t data[4];
+
+	void setRandom()
+	{
+		for (int i = 0; i < 4; i++) {
+			data[i] = 0;
+			for (int j = 0; j < 4; j++) {
+				data[i] = (data[i] << 16) | (rand() & 0xFFFF);
+			}
+		}
+	}
+	void clear()
+	{
+		memset(data, 0, sizeof(data));
+	}
+	Data256() {
+		clear();
+	}
+
+	/** Convert 0-61 uint8 to [0-9A-Za-z]
+	* No argument validation. */
+	static char uint8_to_base62(uint8_t n) {
+		return n + (n < 10 ? '0' : n < 36 ? -10 + 'A' : -36 + 'a');
+	}
+
+	/** Convert [0-9A-Za-z] to 0-61 uint8
+	* No argument validation. */
+	static uint8_t base62_to_uint8(char c) {
+		if ('0' <= c && c <= '9')
+			return c - '0';
+
+		if ('A' <= c && c <= 'Z')
+			return c + (-'A' + 10);
+
+		if ('a' <= c && c <= 'z')
+			return c + (-'a' + 36);
+
+		throw "base62_to_uint8";
+
+		return 0;
+	}
+
+	/** Serialisation 4 * uint64_t to ([0-9A-Za-z]){43}.
+	* No argument validation.
+	* Can get speed up with AVX2 4*uint64_t,
+	* can not use AVX2 8*uint32_t and 63-dimension not help. */
+	void serialize(char* outBuffer43Chars09AZaz) {
+		uint64_t sumOfRemainders = 0;
+
+		/* Each uint64_t is (62 ^ 10) * 21.98. */
+		for (int i = 0, power = 1; i < 4; i++, power *= 22) {
+			uint64_t t = data[i];
+			for (int j = 0; j < 10; j++) {
+				*outBuffer43Chars09AZaz++ = uint8_to_base62(t % 62);
+				t /= 62;
+			}
+			sumOfRemainders += t * power;
+		}
+
+		/* log(22 ^ 3, 62) = 2.996 => 3 symbols */
+		for (int i = 0; i < 3; i++) {
+			*outBuffer43Chars09AZaz++ = uint8_to_base62(sumOfRemainders % 62);
+			sumOfRemainders /= 62;
+		}
+	}
+
+	/** Deserialisation ([0-9A-Za-z]){43} to 4 * uint64_t.
+	* No argument validation. */
+	void deserialize(char* inBuffer43Chars09AZaz) {
+
+		/* Unpack ([0-9A-Za-z]){3} to 4x22. */
+		uint64_t sumOfRemainders = 0;
+		for (int i = 2; i >= 0; i--) {
+			sumOfRemainders = sumOfRemainders * 62 + base62_to_uint8(inBuffer43Chars09AZaz[40 + i]);
+		}
+		for (int i = 3, power = 22 * 22 * 22; i >= 0; i--, power /= 22) {
+			data[i] = sumOfRemainders / power;
+			sumOfRemainders %= power;
+		}
+
+		/* Unpack ([0-9A-Za-z]){40} to 4*(2^32)/21.98 */
+		for (int i = 0; i < 4; i++) {
+			for (int j = 9; j >= 0; j--) {
+				data[i] = data[i] * 62 + base62_to_uint8(inBuffer43Chars09AZaz[i * 10 + j]);
+			}
+		}
+	}
+
+	bool operator==(const Data256& other) const {
+		bool match = true;
+		for (int i = 0; i < 4; i++) {
+			if (data[i] != other.data[i]) {
+				match = false;
+				break;
+			}
+		}
+		return match;
+	}
+
+};
+
+int main() {
+
+	for (int i = 0; i < 62; i++)
+	{
+		char c = Data256::uint8_to_base62(i);
+		uint8_t d = Data256::base62_to_uint8(c);
+		if (d != i)
+		{
+			throw "wrong symbol converter";
+		}
+	}
+
+	for (int cycle = 0; cycle < 1000000; cycle++)
+	{
+		Data256 sourceData;
+		sourceData.setRandom();
+		char serialized[43];
+		sourceData.serialize(serialized);
+
+		Data256 destData;
+		destData.deserialize(serialized);
+
+		if (!(sourceData == destData))
+		{
+			cout << "Source 256 bits in hex:\n";
+			for (int i = 0; i < 4; i++) {
+				cout << hex << sourceData.data[i] << " ";
+			}
+			cout << dec << endl;
+
+			cout << "Serialised 43 bytes [0-9A-Za-z]:\n";
+			for (int i = 0; i < 43; i++) {
+				cout << (int)serialized[i] << " ";
+			}
+			cout << endl;
+
+			cout << "Deserialised 256 bits in hex:\n";
+			for (int i = 0; i < 4; i++) {
+				cout << hex << destData.data[i] << " ";
+			}
+			cout << dec << endl;
+
+			cout << "Data not match!: " << endl;
+
+
+			int a = _getch();
+		}
+	}
+	cout << "If no error messages, program end OK: " << endl;
+
+	return 0;
+}
